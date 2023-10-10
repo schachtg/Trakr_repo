f8d1148745a73dc8748321ca1833aa519c277ddf
Graham Schacht <schachtgraham@gmail.com>
2023-10-09 22:40:48 -0500

    Updated projects page

--- client/src/pages/ProjectsPage/ProjectsPage.js
+++ client/src/pages/ProjectsPage/ProjectsPage.js
@@ -1,9 +1,13 @@
 import React, { Fragment } from 'react';
+import styles from './ProjectsPage.module.css';
 
 export default function ProjectsPage() {
     return (
         <Fragment>
             <h1 className="text-center mt-5">Projects Page</h1>
+            <div className={styles.section_container}>
+                         
+            </div>
         </Fragment>
     );
 }
